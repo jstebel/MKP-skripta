110110ba0cc4ca1f67b52d01ac823f49aae531b2
jstebel <jan.stebel@tul.cz>
2016-12-08 08:32:48 +0100

    Matlab script for 1d fem corrected (wrong jacobian and matrix entries).

--- cviceni/matlab/mkp1d_heat.m
+++ cviceni/matlab/mkp1d_heat.m
@@ -1,26 +1,23 @@
-% Vypocet pruhybu nosniku (1D interval (0,1))
+% Vypocet vedeni tepla (1D interval (x0,x1))
 % metodou konecnych prvku
 %
-% -u'' = -1 v (0,1)
-%  u(0) = u0, u(1) = u1
+% -K*u'' = sigma*(u-u_f) v (x0,x1)
+%  K*u'(x0) = (u_d-u)/epsilon
+% -K*u'(x1) = q
 %
-% u0 = poloha leveho konce nosniku
-% u1 = poloha praveho konce nosniku
-% n  = pocet delicich uzlu intervalu
+% u_d = teplota v bode x0
+% q   = tepelny tok v bode x1
+% n   = pocet delicich uzlu intervalu
 %
-% Navratove hodnoty:
 % x = delici body intervalu
-% u = vypocteny pruhyb
+% u = vypoctena teplota
 % A = matice diskretni ulohy
 % b = vektor diskretni ulohy
 
-%%%%% Vypocet %%%%%
+% Vstupni parametry
 
 x0 = 0;  % leva hranice
 x1 = 1;  % prava hranice
-n  = 10; % pocet elementu
-jac=(x1-x0)/10;
-
 % q
 bc_flux=10; 
 % K
@@ -29,11 +26,16 @@ cond = 2;
 sigma = 0;
 % u_f
 uf = 1;
-% u_0 (Dirichlet)
+% u_d (Dirichlet)
 u0 = 10;
 % epsilon - aproximace Dirichletovy podminky
 epsilon=1e-3;
 
+
+%%%%% Vypocet %%%%%
+n  = 100; % pocet elementu
+
+jac=(x1-x0)/n; % jakobian (velikost elementu)
 
   % Sestaveni matice
   A = zeros(n+1,n+1);
@@ -41,20 +43,20 @@ epsilon=1e-3;
   % vypocet skalarnich soucinu bazovych funkci
   % (pouzivame po castech linearni funkce, tzv. Courantovu bazi)
   i=1
-  A(i,i)   = cond*jac + sigma*jac*(1.0/3.0);    
-  A(i,i+1) = -cond*jac + sigma*jac*(1.0/6.0);
+  A(i,i)   = (cond/jac^2 + sigma*(1.0/3.0))*jac;
+  A(i,i+1) = (-cond/jac^2 + sigma*(1.0/6.0))*jac;
   
-  for i=2:n    
-    A(i,i)   = 2*cond*jac + 2*sigma*jac*(1.0/3.0);    
-    A(i,i-1) = -cond*jac + sigma*jac*(1.0/6.0);
-    A(i,i+1) = -cond*jac + sigma*jac*(1.0/6.0);
+  for i=2:n
+    A(i,i)   = (2*cond/jac^2 + 2*sigma*(1.0/3.0))*jac;
+    A(i,i-1) = (-cond/jac^2 + sigma*(1.0/6.0))*jac;
+    A(i,i+1) = (-cond/jac^2 + sigma*(1.0/6.0))*jac;
   end
   i=n+1
-  A(i,i)   = cond*jac + sigma*jac*(1.0/3.0);    
-  A(i,i-1) = -cond*jac + sigma*jac*(1.0/6.0);
+  A(i,i)   = (cond/jac^2 + sigma*(1.0/3.0))*jac;
+  A(i,i-1) = (-cond/jac^2 + sigma*(1.0/6.0))*jac;
 
   % dirichletovy okrajove podminky
-  A(1,1) = A(1,1)+1/epsilon;  
+  A(1,1) = A(1,1) - 1/epsilon;  
   %A(1,1) = 10; 
   
     
@@ -69,7 +71,8 @@ epsilon=1e-3;
   i=n+1
   b(i,1) = sigma*uf*jac/2;
   % okrajove podminky  
-  b(n+1,1) = b(n+1,1) + bc_flux;
+  b(1,1) = b(1,1) - u0/epsilon;
+  b(n+1,1) = b(n+1,1) - bc_flux;
   
   % Vyreseni algebraicke soustavy
   u = A\b;
