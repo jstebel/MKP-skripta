0b51af906a892144c265040fb66ed72e7975a3c0
Paulie14 <pavelexner@gmail.com>
2022-12-16 15:25:16 +0100

    Update norm convergence script.

--- cviceni/matlab-Pavel/norm_convergence.m
+++ cviceni/matlab-Pavel/norm_convergence.m
@@ -5,29 +5,16 @@ clc;
 func_1 = @(x,n) x.^n;
 func_2 = @(x,n) x.^2 + x/n;
 
-func_3 = @(x,n) ifelse( x<1.0/(n^3) , n, x.^(-1.0/3.0));
-%function y = func_3(x,n)
-%  if(length(x) == 1)
-%    if(x <= 1/n^3)
-%      y = n;
-%    else
-%      y = x^(-1.0/3.0);
-%    end
-%  else
-%  y=1
-%    y = zeros(length(x));
-%    for i=1:length(x)
-%      if(x(i) <= 1/n^3)
-%        y(i) = n;
-%      else
-%        y(i) = x(i)^(-1.0/3.0);
-%      end
-%    end
-%  end
-%end
+% func_3 = @(x,n) ifelse( x<1.0/(n^3) , n, x.^(-1.0/3.0));
+
 
 func_4 = @(x,n) exp(-x).*cos(x/n);
 
+
+% plot_convergence(func_1,0,1,20);
+% plot_convergence(func_2,-1,1,20);
+plot_convergence(@func_3,0,1,5);
+% plot_convergence(func_4,-1,1,20);
 
 function y = plot_convergence(func,a,b,N)
   x = linspace(a,b,1000);
@@ -39,7 +26,22 @@ function y = plot_convergence(func,a,b,N)
   hold off;  
 end
 
-plot_convergence(func_1,0,1,20);
-plot_convergence(func_2,-1,1,20);
-plot_convergence(func_3,0,1,5);
-plot_convergence(func_4,-1,1,20);
+function y = func_3(x,n)
+ if(length(x) == 1)
+   if(x <= 1/n)
+     y = -2*n;
+   else
+     y = -2*x^(-1.0);
+   end
+ else
+ y=1
+   y = zeros(length(x));
+   for i=1:length(x)
+     if(x(i) <= 1/n)
+       y(i) = -2*n;
+     else
+       y(i) = -2*x(i)^(-1.0);
+     end
+   end
+ end
+end
